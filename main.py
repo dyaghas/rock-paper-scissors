98a8022e38a858511d7d98ad8444c0af9fa224b4
Gustavo Cristian <diamonddyaghas@gmail.com>
2022-08-09 11:26:21 -0300

    loop added

--- main.py
+++ main.py
@@ -5,39 +5,47 @@ def get_choices():
 
     wins = 0
     losses = 0
+    carry_on = 1
 
-    possible_movements = ["rock", "paper", "scissors"]
-    computer_choice = random.choice(possible_movements)
-    player_choice = input("Choose rock, paper, scissors: ")
-    choices = {"player": player_choice, "computer": computer_choice}
+    while carry_on == 1:
 
-    if player_choice in possible_movements:
+        possible_movements = ["rock", "paper", "scissors"]
+        computer_choice = random.choice(possible_movements)
+        player_choice = input("Choose rock, paper, scissors: ")
+        choices = {"player": player_choice, "computer": computer_choice}
 
-        print("player choice: " + choices.get("player") + "\ncomputer choice: " + choices.get("computer"))
+        if player_choice in possible_movements:
 
-        if choices.get("player") == "paper" and choices.get("computer") == "rock":
-            wins += 1
-            print("You won!")
+            print("player choice: " + choices.get("player") + "\ncomputer choice: " + choices.get("computer"))
 
-        elif choices.get("player") == "rock" and choices.get("computer") == "scissors":
-            wins += 1
-            print("You won!")
+            if choices.get("player") == "paper" and choices.get("computer") == "rock":
+                wins += 1
+                print("You won!")
 
-        elif choices.get("player") == "scissors" and choices.get("computer") == "paper":
-            wins += 1
-            print("You won!")
+            elif choices.get("player") == "rock" and choices.get("computer") == "scissors":
+                wins += 1
+                print("You won!")
+
+            elif choices.get("player") == "scissors" and choices.get("computer") == "paper":
+                wins += 1
+                print("You won!")
+
+            else:
+                losses += 1
+                print("you lost")
+
+            scoreboard = "scoreboard: {} / {}".format(wins, losses)
+            print(scoreboard)
 
         else:
-            losses += 1
-            print("you lost")
+            print("Type a valid option")
 
-        scoreboard = "scoreboard: {} / {}".format(wins, losses)
-        print(scoreboard)
+        continue_choice = input("Do you want to play another round? yes / no \n")
 
-    else:
-        print("Type a valid option")
-
-    return choices
+        if continue_choice == "yes":
+            carry_on = 1
+        else:
+            carry_on = 0
 
 
 get_choices()
